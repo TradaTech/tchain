8f1b4876801d60a5e19590472c3396e798df338b
Truong Hong Thi <thi1981@gmail.com>
2019-02-12 19:03:54 +0700

    Fix Symbol error

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/api/call', async function(req, res) {
             data: result
         });
     } catch (error) {
-        //console.log(error)
+        console.log(error)
         res.json({
             success: false,
             error: String(error)
